243699e66da64f2ef8cf43db4efc98e648494cd4
sellbell07@gmail.com <sellbell07@gmail.com>
2024-12-19 21:30:29 +0900

    init: 초기세팅
    - 페이지, 구성 요소 및 레이아웃을 위한 폴더 구조 추가
    - 메인 레이아웃과 동적 라우팅이 포함된 React 라우터 구성
    - 초기세팅 요소는 package.json 확인

---
+++ zumul-app/src/layouts/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Footer(props) {
+  return (
+    <>
+    <h2>footer</h2>
+    </>
+    
+  );
+}
+
+export default Footer;
---
+++ zumul-app/src/layouts/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Header(props) {
+    return (
+        <header>
+            <h1>header</h1>
+            
+            {/* 1. 로고 h1 */}
+            {/* 2. 검색바 */}
+            {/* 3. icon 버튼(faivorit, cart) */}
+            {/* 4. user 메뉴 로그인 전: 회원가입, 로그인 || 로그인(일반): 마이페이지, 로그아웃 || 로그인(판매자): 판매관리 추가*/}
+
+        </header>
+    );
+}
+
+export default Header;
